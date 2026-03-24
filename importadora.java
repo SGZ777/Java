aec376db6ba99faf67a0ee3806eed4c94fe9173e
Arthur <arthursgz77@gmail.com>
2026-03-24 14:04:45 -0300

    Implement product import functionality in Main class
    Added a Main class for product import calculations.

--- importadora.java
+++ importadora.java
@@ -6,8 +6,36 @@ package com.mycompany.mavenproject1;
 
 /**
  *
- * @author 26175437
+ * @author Arthur
  */
-public class importadora {
-    
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String descricao = "";
+        double precoEmDolar = 0;
+        int quantidadeProduto = 0;
+        double valorDolar = 5.24;
+        double subtotalDolar = 0;
+        double subtotalReais = 0;
+        double valorEmReal = 0;
+
+        System.out.println("Digite a descrição do produto: ");
+        descricao = sc.nextLine();
+        System.out.println("Digite o valor do produto em dólares: ");
+        precoEmDolar = Double.parseDouble(sc.nextLine());
+        System.out.println("Digite a quantidade de produtos: ");
+        quantidadeProduto = Integer.parseInt(sc.nextLine());
+        subtotalDolar = quantidadeProduto * precoEmDolar;
+        valorEmReal = valorDolar * precoEmDolar;
+        subtotalReais = quantidadeProduto * valorEmReal;
+
+        System.out.println("Produto: " + descricao);
+        System.out.println("Valor em US$: " + precoEmDolar);
+        System.out.println("Valor em R$: " + valorEmReal);
+        System.out.println("Subtotal em US$: " + subtotalDolar);
+        System.out.println("Subtotal em R$: " + subtotalReais);
+    }
 }
