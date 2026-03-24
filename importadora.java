3f597ea6624c525e33488bea6285ea4f646ba5ca
Arthur <arthursgz77@gmail.com>
2026-03-24 14:30:43 -0300

    update importadora
    Reorganized code structure by adding comments for inputs, processing, and outputs.

--- importadora.java
+++ importadora.java
@@ -1,18 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.mavenproject1;
-
-/**
- *
+/*
  * @author Arthur
  */
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        //ENTRADAS
         Scanner sc = new Scanner(System.in);
         String descricao = "";
         double precoEmDolar = 0;
@@ -21,17 +15,17 @@ public class Main {
         double subtotalDolar = 0;
         double subtotalReais = 0;
         double valorEmReal = 0;
-
         System.out.println("Digite a descrição do produto: ");
         descricao = sc.nextLine();
         System.out.println("Digite o valor do produto em dólares: ");
         precoEmDolar = Double.parseDouble(sc.nextLine());
         System.out.println("Digite a quantidade de produtos: ");
         quantidadeProduto = Integer.parseInt(sc.nextLine());
+        //PROCESSAMENTO
         subtotalDolar = quantidadeProduto * precoEmDolar;
         valorEmReal = valorDolar * precoEmDolar;
         subtotalReais = quantidadeProduto * valorEmReal;
-
+        //SAÍDAS
         System.out.println("Produto: " + descricao);
         System.out.println("Valor em US$: " + precoEmDolar);
         System.out.println("Valor em R$: " + valorEmReal);
