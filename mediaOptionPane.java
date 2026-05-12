5a67b782284fc56e2d0988fd16aaf28a2b5fb7bb
Arthur <arthursgz77@gmail.com>
2026-05-11 22:55:14 -0300

    update mediaOptionPane.java

--- mediaOptionPane.java
+++ mediaOptionPane.java
@@ -1,11 +1,14 @@
+package com.mycompany.mavenproject1;
+
 import javax.swing.JOptionPane;
 
 public class mediaOptionPane {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         String nome;
         String situacaoAluno;
         String alunoMaiorNota = "";
         double mediaGeral = 0;
+        double somaMedias = 0;
         double n1;
         double n2;
         int opcao = 0;
@@ -13,36 +16,37 @@ public class mediaOptionPane {
         double maiorNota = 0;
 
         while (opcao != 1){
-                JOptionPane.showMessageDialog(null, "Calculo de média", "Calculo de média", JOptionPane.INFORMATION_MESSAGE);
-                nome = JOptionPane.showInputDialog(null, "Digite seu nome: ");
-                n1 =  Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a primeira nota: "));
-                n2 =  Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a segunda nota: "));
-                double media = (n1 + n2) / 2;
-                qtdAlunos++;
-                mediaGeral = (mediaGeral + media) / qtdAlunos;
+            JOptionPane.showMessageDialog(null, "Calculo de média", "Calculo de média", JOptionPane.INFORMATION_MESSAGE);
+            nome = JOptionPane.showInputDialog(null, "Digite seu nome: ");
+            n1 =  Double.parseDouble(JOptionPane.showInputDialog(null, "Digite a primeira nota: "));
+            n2 =  Double.parseDouble(JOptionPane.showInputDialog(null, "Digite a segunda nota: "));
+            double media = (n1 + n2) / 2;
+            qtdAlunos++;
+            somaMedias += media;
+            mediaGeral = somaMedias / qtdAlunos;
 
-                if (media > maiorNota){
-                    maiorNota = media;
-                    alunoMaiorNota = nome;
-                }
-                if (media >= 50){
-                    
-                    situacaoAluno = "Aprovado";
-                }
-                else {
-                    situacaoAluno = "Reprovado";
-                }
+            if (media > maiorNota){
+                maiorNota = media;
+                alunoMaiorNota = nome;
+            }
+            if (media >= 50){
 
-                JOptionPane.showMessageDialog(null,"Aluno: " + nome + "\nMédia: " + media + " - " + situacaoAluno);
-                JOptionPane.showMessageDialog(null, "Média geral: " + mediaGeral + "\nMaior nota: " + maiorNota + " tirada por " + alunoMaiorNota);
-                opcao = JOptionPane.showConfirmDialog(null, "Deseja incluir mais alunos?", "Pergunta", JOptionPane.YES_NO_OPTION);
-                switch (opcao){
-                    case 0:
-                        break;
-                    case 1:
-                        JOptionPane.showMessageDialog(null, "Até mais :)");
-                        break;
-                }
+                situacaoAluno = "Aprovado";
+            }
+            else {
+                situacaoAluno = "Reprovado";
+            }
+
+            JOptionPane.showMessageDialog(null,"Aluno: " + nome + "\nMédia: " + media + " - " + situacaoAluno);
+            JOptionPane.showMessageDialog(null, "Média geral: " + mediaGeral + "\nMaior nota: " + maiorNota + " tirada por " + alunoMaiorNota);
+            opcao = JOptionPane.showConfirmDialog(null, "Deseja incluir mais alunos?", "Pergunta", JOptionPane.YES_NO_OPTION);
+            switch (opcao){
+                case 0:
+                    break;
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Até mais :)");
+                    break;
+            }
         }
     }
 }
