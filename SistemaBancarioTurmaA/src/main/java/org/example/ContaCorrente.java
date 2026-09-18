1689d07c63a976f3c7394ea64561b3880dab0f61
Arthur <arthursgz77@gmail.com>
2026-09-17 21:04:17 -0300

    banco pronto

--- SistemaBancarioTurmaA/src/main/java/org/example/ContaCorrente.java
+++ SistemaBancarioTurmaA/src/main/java/org/example/ContaCorrente.java
@@ -1,4 +1,45 @@
 package org.example;
 
-public class ContaCorrente {
+public class ContaCorrente extends ContaBancaria{
+    private double limite;
+
+    public ContaCorrente(double limite) {
+        this.limite = limite;
+    }
+
+    public ContaCorrente(String titular, int numero, double limite){
+        super(numero, titular);
+        this.limite = limite;
+    }
+
+    public double getLimite(){
+        return limite;
+    }
+    public void setLimite(double limite){
+        this.limite = limite;
+    }
+
+    @Override
+    public void sacar(double valor){
+        if (valor <= 0){
+            System.out.println("Valor de saque inválido!");
+        }
+        else {
+            if (valor <= getSaldo() + limite){
+                double tempSaldo = getSaldo();
+                if (valor <= tempSaldo){
+                    super.sacar(valor);
+                }
+                else{
+                    double valorLimite = valor - tempSaldo;
+                    super.sacar(tempSaldo);
+                    limite -= valorLimite;
+                    System.out.println("Saque realizado utilizando limite!");
+                }
+            }
+            else{
+                System.out.println("Saque não realizado! Saldo + limte insuficientes");
+            }
+        }
+    }
 }
--- SistemaBancarioTurmaA/src/main/java/org/example/Main.java
+++ SistemaBancarioTurmaA/src/main/java/org/example/Main.java
@@ -3,132 +3,264 @@ package org.example;
 import javax.swing.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        menuPrincipal();
+        ContaBancaria conta1 = new ContaBancaria(1, "Kentaro Miura");
+        menuPrincipal(conta1);
+
     }
 
-    public static void menuPrincipal() {
-
+    public static void menuPrincipal(ContaBancaria conta1) {
         int opcao;
-
         do {
             opcao = Integer.parseInt(
                     JOptionPane.showInputDialog(
                             null,
-                            "----------------- BANCO MASTER -----------------\n\n" +
+                            " * * CAIXA ELETRÔNICO * * \n\n" +
                                     "1. Conta Bancária\n" +
                                     "2. Conta Corrente\n" +
                                     "3. Conta Poupança\n" +
-                                    "4. Sair\n\n" +
-                                    "Digite a opção desejada:"
+                                    "0. Sair\n\n" +
+                                    "Escolha uma opção: "
+                    )
+            );
+            switch (opcao) {
+                case 1: {
+                    menuContaBancaria(conta1);
+                    break;
+                }
+                case 2: {
+                    menuContaCorrente();
+                    break;
+                }
+                case 3: {
+                    menuContaPoupanca();
+                    break;
+                }
+                case 0: {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Obrigado por usar os serviços do DB Bank."
+                    );
+                    break;
+                }
+                default: {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Opção Inválida!"
+                    );
+                }
+            }
+        } while (opcao != 0);
+    }
+
+
+    // ================= CONTA BANCÁRIA =================
+
+    public static void menuContaBancaria(ContaBancaria conta1) {
+        int menu2;
+        double valor;
+        do {
+            menu2 = Integer.parseInt(
+                    JOptionPane.showInputDialog(
+                            null,
+                            " * * MENU CONTA BANCÁRIA * *\n\n" +
+                                    "1. Ver Saldo\n" +
+                                    "2. Sacar\n" +
+                                    "3. Depositar\n" +
+                                    "0. Voltar ao menu anterior\n\n" +
+                                    "Escolha a opção desejada: "
+                    )
+            );
+            switch (menu2) {
+
+                case 1: { // Ver saldo
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Cliente: " + conta1.getTitular() + "\n" +
+                                    "Número: " + conta1.getNumero() + "\n" +
+                                    "Saldo: R$ " + conta1.getSaldo()
+                    );
+                    break;
+                }
+                case 2: { // Sacar
+                    valor = Double.parseDouble(
+                            JOptionPane.showInputDialog(
+                                    null,
+                                    "Digite valor a sacar: "
+                            )
+                    );
+                    conta1.sacar(valor);
+                    break;
+                }
+                case 3: { // Depositar
+                    valor = Double.parseDouble(
+                            JOptionPane.showInputDialog(
+                                    null,
+                                    "Digite valor a depositar: "
+                            )
+                    );
+                    conta1.depositar(valor);
+                    break;
+                }
+                case 0: { // Voltar
+                    break;
+                }
+                default: {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Opção Inválida!"
+                    );
+                }
+            }
+        } while (menu2 != 0);
+    }
+
+    public static void menuContaPoupanca() {
+        ContaPoupanca conta3 = new ContaPoupanca(1, "Daniel", 5);
+        int opcao;
+        double valor;
+        do {
+
+            opcao = Integer.parseInt(
+                    JOptionPane.showInputDialog(
+                            null,
+                            " * * CONTA POUPANÇA * * \n\n" +
+                                    "1. Consultar saldo\n" +
+                                    "2. Sacar\n" +
+                                    "3. Depositar\n" +
+                                    "4. Aplicar Rendimento\n\n" +
+                                    "0. Voltar ao menu anterior\n\n" +
+                                    "Digite a opção desejada: "
                     )
             );
 
             switch (opcao) {
-                case 1: {
-                    JOptionPane.showMessageDialog(
-                            null,
-                            "Menu conta bancária"
-                    );
-                    menuContaBancaria();
-                    break;
-                }
 
-                case 2: {
+                case 1: { // Consultar saldo
                     JOptionPane.showMessageDialog(
                             null,
-                            "Menu conta corrente"
+                            "Conta Número: " + conta3.getNumero() + "\n" +
+                                    "Titular: " + conta3.getTitular() + "\n" +
+                                    "Saldo R$: " + conta3.getSaldo()
                     );
                     break;
                 }
 
-                case 3: {
-                    JOptionPane.showMessageDialog(
-                            null,
-                            "Menu conta poupança"
+                case 2: { // Sacar
+                    valor = Double.parseDouble(
+                            JOptionPane.showInputDialog(
+                                    null,
+                                    "Digite valor do saque: "
+                            )
                     );
+                    conta3.sacar(valor);
                     break;
                 }
 
-                case 4: {
-                    JOptionPane.showMessageDialog(
-                            null,
-                            "Até logo!"
+                case 3: { // Depositar
+
+                    valor = Double.parseDouble(
+                            JOptionPane.showInputDialog(
+                                    null,
+                                    "Digite valor do depósito: "
+                            )
                     );
+                    conta3.depositar(valor);
                     break;
                 }
 
+                case 4: { // Aplicar rendimento
+                    conta3.aplicarRendimento();
+                    break;
+                }
+                case 0: { // Voltar
+                    break;
+                }
                 default: {
                     JOptionPane.showMessageDialog(
                             null,
-                            "Opção inválida"
+                            "Opção Inválida!"
                     );
                 }
             }
 
-        } while (opcao != 4);
+        } while (opcao != 0);
     }
 
-    public static void menuContaBancaria(){
-        ContaBancaria conta1 = new ContaBancaria(1, "Daniel Vorcaro");
-
+    public static void menuContaCorrente() {
+        ContaCorrente conta2 = new ContaCorrente("Medina", 2, 1000);
         int opcao;
         double valor;
-
         do {
             opcao = Integer.parseInt(
                     JOptionPane.showInputDialog(
                             null,
-                            "----------------- BANCO MASTER -----------------\n\n" +
-                                    "1. Consultar Saldo\n" +
-                                    "2. Depositar\n" +
-                                    "3. Sacar\n" +
-                                    "4. Voltar\n\n" +
-                                    "Digite a opção desejada:"
+                            " * * CONTA CORRENTE * * \n\n" +
+                                    "1. Consultar saldo\n" +
+                                    "2. Sacar\n" +
+                                    "3. Depositar\n" +
+                                    "4. Consultar limite\n\n" +
+                                    "0. Voltar ao menu anterior\n\n" +
+                                    "Digite a opção desejada: "
                     )
             );
 
             switch (opcao) {
-                case 1: {
+                case 1: { // Consultar saldo
                     JOptionPane.showMessageDialog(
                             null,
-                            "Saldo: R$" + conta1.getSaldo()
+                            "Conta Número: " + conta2.getNumero() + "\n" +
+                                    "Titular: " + conta2.getTitular() + "\n" +
+                                    "Saldo R$: " + conta2.getSaldo()
                     );
                     break;
                 }
 
-                case 2: {
+                case 2: { // Sacar
+                    valor = Double.parseDouble(
+                            JOptionPane.showInputDialog(
+                                    null,
+                                    "Digite valor do saque: "
+                            )
+                    );
+                    conta2.sacar(valor);
+                    break;
+                }
+
+                case 3: { // Depositar
+                    valor = Double.parseDouble(
+                            JOptionPane.showInputDialog(
+                                    null,
+                                    "Digite valor do depósito: "
+                            )
+                    );
+                    conta2.depositar(valor);
+                    break;
+                }
+
+                case 4: { // Consultar limite
                     JOptionPane.showMessageDialog(
                             null,
-                            "Depositar"
+                            "Conta Número: " + conta2.getNumero() + "\n" +
+                                    "Titular: " + conta2.getTitular() + "\n" +
+                                    "Saldo R$: " + conta2.getSaldo() + "\n" +
+                                    "Limite R$: " + conta2.getLimite()
                     );
-                    valor = Integer.parseInt(JOptionPane.showInputDialog("Qual valor a ser depositado?"));
-                    conta1.depositar(valor);
                     break;
                 }
+                case 0: { // Voltar
 
-                case 3: {
-                    JOptionPane.showMessageDialog(
-                            null,
-                            "Sacar"
-                    );
-                    valor = Integer.parseInt(JOptionPane.showInputDialog("Qual valor a ser depositado?"));
-                    conta1.sacar(valor);
                     break;
                 }
-
-                case 4: {
-                    break;
-                }
-
                 default: {
                     JOptionPane.showMessageDialog(
                             null,
-                            "Opção inválida"
+                            "Opção Inválida!"
                     );
                 }
             }
 
-        } while (opcao != 4);
+        } while (opcao != 0);
     }
 }
